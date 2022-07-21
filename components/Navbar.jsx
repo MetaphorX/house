70ae657c9dddc1f5393cb3ef5b18fc6fc2b09121
Shakur <gall.abdulshakur@gmail.com>
2022-07-21 02:47:04 +0100

    reworked the frontend

---
+++ components/Navbar.jsx
@@ -0,0 +1,103 @@
+import React, {useState} from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+import {AiOutlineMenu, AiOutlineClose, AiOutlineMail, AiOutlineFacebook, AiOutlineTwitter} from 'react-icons/ai'
+import {FaLinkedinIn} from 'react-icons/fa'
+
+const Navbar = () => {
+
+    const [nav, setNav] = useState(false)
+    const navHandler=()=>{
+        setNav(!nav)
+    }
+  return (
+    <div className='fixed w-full z-[100]'>
+        <div className='flex justify-between items-center shadow-xl w-full h-full px-2 2xl:px-16'>
+            <Image src="/../public/assets/logo.png" width={125} height={100}/>
+            <div className='md:flex space-x-5'>
+                <ul className='hidden md:flex'>
+                    <Link href="/">
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Home</li>
+                    </Link>
+                    <Link href="/">
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Rent Lodge</li>
+                    </Link>
+                    <Link href="/">
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Agents</li>
+                    </Link>
+                    <Link href="/">
+                        <li className='ml-10 text-sm uppercase hover:border-b'>About</li>
+                    </Link>
+                    <Link href="/">
+                        <li className='ml-10 text-sm uppercase hover:border-b'>Contact Us</li>
+                    </Link>
+                    <Link href="/">
+                        <li className='ml-10 text-sm p-3 px-6 pt-2 text-white bg-red-500 rounded-full baseline cursor-pointer'>Sign in</li>
+                    </Link>
+                </ul>
+                <div onClick={navHandler} className='md:hidden cursor-pointer'>
+                    <AiOutlineMenu fontSize={35}/>
+                </div>
+            </div>
+        </div>
+        <div className={nav ? 'md:hidden fixed left-0 top-0 w-full h-screen bg-black/70' : ''}>
+            <div className={
+                nav ? 'fixed left-0 top-0 w-[75%] sm:w-[60%] md:w-[45%] h-screen bg-white p-10 ease-in duration-500' 
+                    : 'fixed left-[-100%] top-0 p-10 ease-in duration-500'
+                }>
+                <div>
+                    <div className='flex w-full items-center justify-between text-center'>
+                        <h2 className='text-4xl font-bold text-center p-3'>Rent-a-Crib</h2>
+                        <div onClick={navHandler} className='rounded-full hover:shadow-md p-3 cursor-pointer'>
+                            <AiOutlineClose fontSize={35}/>
+                        </div>
+                    </div>
+                    <div className='border-b my-4 mt-5 text-center'>
+                        <p className='w-[85%] md:w-[90%] py-4'>Get Accomodations before Resumption. <br/>Secure and Guaranteed</p>
+                    </div>
+                </div>
+                <div className='py-4 flex flex-col'>
+                    <ul className='text-center uppercase text-lg'>
+                        <Link href="/">
+                            <li className='py-4 cursor-pointer hover:scale-110 ease-in duration-20'>Home</li>
+                        </Link>
+                        <Link href="/">
+                            <li className='py-4 cursor-pointer hover:scale-110 ease-in duration-20'>Rent Lodge</li>
+                        </Link>
+                        <Link href="/">
+                            <li className='py-4 cursor-pointer hover:scale-110 ease-in duration-20'>Agents</li>
+                        </Link>
+                        <Link href="/">
+                            <li className='py-4 cursor-pointer hover:scale-110 ease-in duration-20'>About</li>
+                        </Link>
+                        <Link href="/">
+                            <li className='ml-10 text-md p-4 m-4 text-center pt-2 text-white bg-red-500 rounded-full baseline cursor-pointer hover:shadow-xl'>Sign in</li>
+                        </Link>
+                    </ul>
+                    <div className='pt-2 text-2xl'>
+                        <p>Connect with us on :</p>
+                        <div className='flex items-center justify-between my-4 w-full sm:w-[80%]'>
+                            <div className='rounded-full shadow-md p-3 cursor-pointer hover:scale-105 duration-20 text-red-500'>
+                                <FaLinkedinIn />
+                            </div>
+                            <div className='rounded-full shadow-md p-3 cursor-pointer hover:scale-105 duration-20 text-red-500'>
+                                <AiOutlineMail />
+                            </div>
+                            <div className='rounded-full shadow-md p-3 cursor-pointer hover:scale-105 duration-20 text-red-500'>
+                                <AiOutlineFacebook />
+                            </div>
+                            <div className='rounded-full shadow-md p-3 cursor-pointer hover:scale-105 duration-20 text-red-500'>
+                                <AiOutlineTwitter />
+                            </div>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+        </div>
+
+    </div>
+  )
+}
+
+export default Navbar
---
+++ pages/property/[slug].js
@@ -0,0 +1,68 @@
+import { client } from "../../lib/client"
+
+
+const Property = ({title, area, camp, location, propertyType, mainImage,price,bedrooms,id,host, slug}) =>{
+    return(
+        <div>
+            {title}
+        </div>
+    )
+}
+
+
+
+
+
+
+
+
+export const getServerSideProps = async (pageContext)=>{
+    const pageSlug = pageContext.query.slug
+     
+	const query = `*[_type == "property" && slug.current == $pageSlug][0]{
+		title,
+        area,
+        camp,
+		location,
+		propertyType,
+		mainImage,images,
+		price,
+		bedrooms,
+		description,
+		agent ->{
+			_id,
+			name,
+			slug,
+			image
+		}
+	}`
+
+	const property = await client.fetch(query, {pageSlug})
+
+	if(!property){
+		return {
+			props:null,
+			notFound: true
+		}
+	}
+	else{
+		return{
+			props:{
+				title: property.title,
+                area: property.area,
+                camp: property.camp,
+				location: property.location,
+				propertyType: property.propertyType,
+				mainImage: property.mainImage,
+				images: property.images,
+				price: property.price,
+				bedrooms:property.bedrooms,
+				description: property.description,
+				agent: property.agent
+			}
+		}
+	}
+}
+
+
+export default Property
