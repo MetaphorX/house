bd9141d500430b34d4d8843689a2cc86246600f0
Shakur <gall.abdulshakur@gmail.com>
2022-07-21 12:56:23 +0100

    agent display

--- pages/property/[slug].js
+++ pages/property/[slug].js
@@ -56,10 +56,20 @@ const Property = ({title, area,description,toilet,meter, camp, location, propert
 
                 </dl>
                 </div>
-                <div className="flex -space-x-2 overflow-hidden">
-                    <img className="inline-block h-10 w-10 rounded-full ring-2 ring-white" src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80" alt=""/>
-                    <dd className="pl-5 mt-2 text-lg text-gray-500 font-bold">{host?.name} </dd>
+                {/* Agent Info */}
+                
+                <div class="max-w-sm -space-x-2 bg-white rounded-lg border shadow-lg dark:bg-white-800">
+                    <div class="flex flex-col items-center pb-10">
+                        <img class="mb-3 w-24 h-24 rounded-full shadow-lg" src="/docs/images/people/profile-picture-3.jpg" alt="Bonnie image" />
+                        <h5 class="mb-1 text-xl font-medium text-gray-900 dark:text-white">{host?.name}</h5>
+                        <span class="text-sm text-gray-500 dark:text-gray-400">House Agent</span>
+                        <div class="flex mt-4 space-x-3 lg:mt-6">
+                            <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Add friend</a>
+                            <a href="#" class="inline-flex items-center py-2 px-4 text-sm font-medium text-center text-gray-900 bg-white rounded-lg border border-gray-300 hover:bg-gray-100 focus:ring-4 focus:outline-none focus:ring-gray-200 dark:bg-gray-800 dark:text-white dark:border-gray-600 dark:hover:bg-gray-700 dark:hover:border-gray-700 dark:focus:ring-gray-700">Message</a>
+                        </div>
+                    </div>
                 </div>
+
             </div>
             
             </div> 
